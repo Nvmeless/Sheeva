ccbe09e3617c65b21e12abdd5d5f2d0debcec85a
Jinn <bntostud@gmail.com>
2021-09-29 22:43:31 +0200

    init

--- public/index.php
+++ public/index.php
@@ -8,3 +8,24 @@ $api = new \Sheeva\Api();
 $config = new \Sheeva\ConfigBuilder();
 $api->run()->call;
 $api->render();
+
+
+// $client = new \GuzzleHttp\Client();
+// $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+// include "../Views/header.php";
+// // 
+// echo $response->getStatusCode(); // 200
+// echo "<br/>";
+// echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+// echo "<br/>";
+// echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+// 
+// Send an asynchronous request.
+//http://api.music-story.com/fr/genre/search?name=Rock
+// $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+// $request = new \GuzzleHttp\Psr7\Request('GET', 'http://api.music-story.com/fr/genre/search?name=Rock');
+// $promise = $client->sendAsync($request)->then(function ($response) {
+    // echo 'I completed! ' . $response->getBody();
+// });
+// 
+// $promise->wait();
